5cb3c9e6494a348f2f1de6caaded909b3841d01e
DESKTOP-S61FIR1\doantheanh172tb <doantheanh172tb@gmail.com>
2017-06-28 11:41:23 +0700

    M: form add

--- app/components/NoteForm.js
+++ app/components/NoteForm.js
@@ -3,18 +3,25 @@ import React from 'react';
 class NoteForm extends React.Component{
     constructor(props){
         super(props);
+        this.state = {isAdding: false};
     }
     handleSubmit(e){
         e.preventDefault();//not refresh page
         this.props.handleSubmit(this.refs.txt.value);
         this.refs.txt.value = '';
+        this.toggle();
+    }
+    toggle(){
+        this.state.isAdding = !this.state.isAdding;
+        this.setState(this.state)
     }
     render(){
-        return <form onSubmit={this.handleSubmit.bind(this)}>
+        if(this.state.isAdding) return <form onSubmit={this.handleSubmit.bind(this)}>
             <input type="text" ref="txt" placeholder="Name" />
             <br/>
             <button>Add</button>
         </form>
+        return <button onClick={this.toggle.bind(this)}>+</button>
     }
 }
 
